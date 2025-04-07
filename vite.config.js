bd713d2ff2a8b707c191015f73402e40b01f14f6
Vinay Vicky <7777vigneshvelu@gmail.com>
2025-04-07 21:08:22 +0530

    Instagram-Login-Page-(03-04-2025)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Instagram-Login-page"
+  base:"/Instagram"
   // base: import.meta.env.VITE_BASE_PATH || "/Instagram-Login-page"
 })
